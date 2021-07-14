bbb530d0f78009b408ae5083b830782285722b4e
Bulby <bulbyvr@gmail.com>
2021-07-14 19:10:15 -0400

    neko : ) and change to tink_cli

--- src/Silk.hx
+++ src/Silk.hx
@@ -1,7 +1,11 @@
 package;
 
+import tink.Cli;
+import tink.cli.Rest;
+
 class Silk {
     public static function main() {
+      /*
         var arguments = Sys.args();
         if (arguments.length == 0 || arguments[0] == 'help') {
             Sys.println('Welcome to silk, a thin little wrapper around haxelib that gives some convienence with alternate names. 
@@ -86,7 +90,187 @@ Usage: silk [command] [options]
 				}
 			}
 			Sys.exit(Sys.command('haxelib', arguments));
+      */
+      Cli.process(Sys.args(), new SilkCli()).handle(Cli.exit);
         }
         
+}
+enum Categories {
+  Basic;
+  Information;
+  Development;
+  Misc;
+}
+class SilkCli {
+  @:optional
+  @:alias('y')
+  public var always:Bool;
+	@:optional
+  @:alias('n')
+  public var never:Bool;
+	@:optional
+	@:alias(false)
+  public var flat:Bool;
+	@:optional
+	@:alias(false)
+  public var debug:Bool;
+	@:alias(false)
+	@:optional
+  public var quiet:Bool;
+  @:alias(false)
+	@:optional
+  public var system:Bool;
+	@:optional
+  @:flag('skip-dependencies', 'no-dep')
+  @:alias(false)
+  public var skipdeps:Bool;
+	@:optional
+  @:alias('g')
+  public var global:Bool;
+  @:defaultCommand
+  public function help(rest:Rest<String>) {
+		Sys.command('haxelib', ['help']);
+  }
+  @:command('install', 'i', 'add')
+  public function install(rest:Rest<String>) {
+    var args = cast (rest : Array<String>);
+		var gitRegex = ~/([A-Za-z0-9_\-.]+)@([a-zA-Z0-9-._~:\/?#\[\]@!$&'\(\)*+,;%=]+)/;
+		var versionRegex = ~/([A-Za-z0-9_\-.]+)@((?:[0-9]+)\.(?:[0-9]+)\.(?:[0-9]+)(?:-(?:[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?)/;
+		var githubRegex = ~/([A-Za-z0-9_\-.]+)@(?:github:)?([a-z0-9\-]+\/[a-z0-9\-]+)(?:#([a-z0-9]+))?/;
+		var mercuryRegex = ~/([A-Za-z0-9_\-.]+)@(?:mercury|hg):([a-zA-Z0-9-._~:\/?#\[\]@!$&'\(\)*+,;%=]+)/;
+    var argFlags = getArgsForhaxe();
+    var argumentsToPass = ['install'];
+    if (args[0] != null) {
+      if (versionRegex.match(args[0])) {
+				argumentsToPass.push(versionRegex.matched(1));
+				argumentsToPass.push(versionRegex.matched(2));
+      } else if (mercuryRegex.match(args[0])) {
+        argumentsToPass[0] = 'hg';
+        argumentsToPass.push(mercuryRegex.matched(1));
+        argumentsToPass.push(mercuryRegex.matched(2));
+      } else if (githubRegex.match(args[0])) {
+        argumentsToPass[0] = 'git';
+				var aUrl = 'https://github.com/';
+				aUrl = aUrl + githubRegex.matched(2);
+				if (githubRegex.matched(3) != null) {
+					aUrl += '/tree/' + githubRegex.matched(3);
+				}
+        argumentsToPass.push(githubRegex.matched(1));
+        argumentsToPass.push(aUrl);
+      } else if (gitRegex.match(args[0])) {
+        argumentsToPass[0] = 'git';
+        argumentsToPass.push(gitRegex.matched(1));
+        argumentsToPass.push(gitRegex.matched(2));
+      } else {
+        argumentsToPass.concat(args);
+      }
+
     }
+    argumentsToPass.concat(argFlags);
+    Sys.command('haxelib', argumentsToPass);
+  }
+  @:command('update')
+  public function update(rest:Rest<String>) {
+		Sys.command('haxelib', ['update'].concat(cast rest).concat(getArgsForhaxe()));
+  }
+  @:command('remove', 'rm', 'uninstall')
+  public function remove(rest:Rest<String>) {
+		Sys.command('haxelib', ['remove'].concat(cast rest).concat(getArgsForhaxe()));
+  }
+  @:command('list', 'ls')
+  public function list(rest:Rest<String>) {
+		Sys.command('haxelib', ['list'].concat(cast rest).concat(getArgsForhaxe()));
+  }
+	@:command('set')
+	public function set(rest:Rest<String>) {
+		Sys.command('haxelib', ['set'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+	@:command('search', 'find')
+	public function search(rest:Rest<String>) {
+		Sys.command('haxelib', ['search'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+	@:command('info', 'about')
+	public function info(rest:Rest<String>) {
+		Sys.command('haxelib', ['info'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+	@:command('config')
+	public function config(rest:Rest<String>) {
+		Sys.command('haxelib', ['config'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+	@:command('path')
+	public function path(rest:Rest<String>) {
+		Sys.command('haxelib', ['path'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+	@:command('libpath')
+	public function libpath(rest:Rest<String>) {
+		Sys.command('haxelib', ['libpath'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+	@:command('version')
+	public function version(rest:Rest<String>) {
+		Sys.command('haxelib', ['version'].concat(cast rest).concat(getArgsForhaxe()));
+    Sys.println('Silk Version: 0.0.1');
+	}
+	@:command('submit')
+	public function submit(rest:Rest<String>) {
+		Sys.command('haxelib', ['submit'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+	@:command('register')
+	public function register(rest:Rest<String>) {
+		Sys.command('haxelib', ['register'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+	@:command('dev')
+	public function dev(rest:Rest<String>) {
+		Sys.command('haxelib', ['dev'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+	@:command('setup')
+	public function setup(rest:Rest<String>) {
+		Sys.command('haxelib', ['setup'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+	@:command('newrepo')
+	public function newrepo(rest:Rest<String>) {
+		Sys.command('haxelib', ['newrepo'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+	@:command('deleterepo')
+	public function delrepo(rest:Rest<String>) {
+		Sys.command('haxelib', ['deleterepo'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+	@:command('convertxml')
+	public function convertxml(rest:Rest<String>) {
+		Sys.command('haxelib', ['convertxml'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+	@:command('run')
+	public function run(rest:Rest<String>) {
+		Sys.command('haxelib', ['run'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+	@:command('proxy')
+	public function proxy(rest:Rest<String>) {
+		Sys.command('haxelib', ['proxy'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+  function getArgsForhaxe():Array<String> {
+		var flagsAsArgs = [];
+		if (always) {
+			flagsAsArgs.push('--always');
+		}
+		if (never) {
+			flagsAsArgs.push('--never');
+		}
+		if (flat) {
+			flagsAsArgs.push('--flat');
+		}
+		if (debug) {
+			flagsAsArgs.push('--debug');
+		}
+		if (quiet) {
+			flagsAsArgs.push('--quiet');
+		}
+		if (system) {
+			flagsAsArgs.push('--system');
+		}
+		if (skipdeps)
+			flagsAsArgs.push('--skip-dependencies');
+		if (global)
+			flagsAsArgs.push('--global');
+    return flagsAsArgs;
+  }
+  public function new() {}
 }
