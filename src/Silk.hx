2a38d1448d7fc900c2f2aca579aaf6c6d80bbd19
Bulby <bulbyvr@gmail.com>
2021-07-14 22:10:51 -0400

    SILK WHY :+1: :100:

--- src/Silk.hx
+++ src/Silk.hx
@@ -1,5 +1,11 @@
 package;
 
+import sys.FileSystem;
+import haxe.io.Path;
+import sys.io.File;
+import haxelib.Data;
+import haxelib.client.Main as HaxelibMain;
+import haxe.remoting.Proxy;
 import tink.Cli;
 import tink.cli.Rest;
 
@@ -101,86 +107,90 @@ enum Categories {
   Development;
   Misc;
 }
+class SiteProxy extends Proxy<haxelib.SiteApi> {}
+@:access(haxelib.client.Main)
 class SilkCli {
-  @:optional
-  @:alias('y')
-  public var always:Bool;
+	var hecks:HaxelibMain;
 	@:optional
-  @:alias('n')
-  public var never:Bool;
+  	@:alias('y')
+	public var always:Bool;
 	@:optional
-	@:alias(false)
-  public var flat:Bool;
+	@:alias('n')
+	public var never:Bool;
 	@:optional
 	@:alias(false)
-  public var debug:Bool;
+	public var flat:Bool;
+	@:optional
+	@:alias(false)
+	public var debug:Bool;
 	@:alias(false)
 	@:optional
-  public var quiet:Bool;
-  @:alias(false)
+	public var quiet:Bool;
+	@:alias(false)
 	@:optional
-  public var system:Bool;
+	public var system:Bool;
 	@:optional
-  @:flag('skip-dependencies', 'no-dep')
-  @:alias(false)
-  public var skipdeps:Bool;
+	@:flag('skip-dependencies', 'no-dep')
+	@:alias(false)
+	public var skipdeps:Bool;
 	@:optional
-  @:alias('g')
-  public var global:Bool;
-  @:defaultCommand
-  public function help(rest:Rest<String>) {
-		Sys.command('haxelib', ['help']);
-  }
-  @:command('install', 'i', 'add')
-  public function install(rest:Rest<String>) {
-    var args = cast (rest : Array<String>);
-		var gitRegex = ~/([A-Za-z0-9_\-.]+)@([a-zA-Z0-9-._~:\/?#\[\]@!$&'\(\)*+,;%=]+)/;
-		var versionRegex = ~/([A-Za-z0-9_\-.]+)@((?:[0-9]+)\.(?:[0-9]+)\.(?:[0-9]+)(?:-(?:[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?)/;
-		var githubRegex = ~/([A-Za-z0-9_\-.]+)@(?:github:)?([a-z0-9\-]+\/[a-z0-9\-]+)(?:#([a-z0-9]+))?/;
-		var mercuryRegex = ~/([A-Za-z0-9_\-.]+)@(?:mercury|hg):([a-zA-Z0-9-._~:\/?#\[\]@!$&'\(\)*+,;%=]+)/;
-    var argFlags = getArgsForhaxe();
-    var argumentsToPass = ['install'];
-    if (args[0] != null) {
-      if (versionRegex.match(args[0])) {
-				argumentsToPass.push(versionRegex.matched(1));
-				argumentsToPass.push(versionRegex.matched(2));
-      } else if (mercuryRegex.match(args[0])) {
-        argumentsToPass[0] = 'hg';
-        argumentsToPass.push(mercuryRegex.matched(1));
-        argumentsToPass.push(mercuryRegex.matched(2));
-      } else if (githubRegex.match(args[0])) {
-        argumentsToPass[0] = 'git';
-				var aUrl = 'https://github.com/';
-				aUrl = aUrl + githubRegex.matched(2);
-				if (githubRegex.matched(3) != null) {
-					aUrl += '/tree/' + githubRegex.matched(3);
-				}
-        argumentsToPass.push(githubRegex.matched(1));
-        argumentsToPass.push(aUrl);
-      } else if (gitRegex.match(args[0])) {
-        argumentsToPass[0] = 'git';
-        argumentsToPass.push(gitRegex.matched(1));
-        argumentsToPass.push(gitRegex.matched(2));
-      } else {
-        argumentsToPass.concat(args);
-      }
+	@:alias('g')
+	public var global:Bool;
+	@:defaultCommand
+	public function help(rest:Rest<String>) {
+			Sys.command('haxelib', ['help']);
+	}
+	@:command('install', 'i', 'add')
+	public function install(rest:Rest<String>) {
+		var args = cast (rest : Array<String>);
+			var gitRegex = ~/([A-Za-z0-9_\-.]+)@([a-zA-Z0-9-._~:\/?#\[\]@!$&'\(\)*+,;%=]+)/;
+			var versionRegex = ~/([A-Za-z0-9_\-.]+)@((?:[0-9]+)\.(?:[0-9]+)\.(?:[0-9]+)(?:-(?:[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?)/;
+			var githubRegex = ~/([A-Za-z0-9_\-.]+)@(?:github:)?([a-z0-9\-]+\/[a-z0-9\-]+)(?:#([a-z0-9]+))?/;
+			var mercuryRegex = ~/([A-Za-z0-9_\-.]+)@(?:mercury|hg):([a-zA-Z0-9-._~:\/?#\[\]@!$&'\(\)*+,;%=]+)/;
+		var argFlags = getArgsForhaxe();
+		var argumentsToPass = ['install'];
+		if (args[0] != null) {
+		if (versionRegex.match(args[0])) {
+					argumentsToPass.push(versionRegex.matched(1));
+					argumentsToPass.push(versionRegex.matched(2));
+		} else if (mercuryRegex.match(args[0])) {
+			argumentsToPass[0] = 'hg';
+			argumentsToPass.push(mercuryRegex.matched(1));
+			argumentsToPass.push(mercuryRegex.matched(2));
+		} else if (githubRegex.match(args[0])) {
+			argumentsToPass[0] = 'git';
+					var aUrl = 'https://github.com/';
+					aUrl = aUrl + githubRegex.matched(2);
+					if (githubRegex.matched(3) != null) {
+						aUrl += '/tree/' + githubRegex.matched(3);
+					}
+			argumentsToPass.push(githubRegex.matched(1));
+			argumentsToPass.push(aUrl);
+		} else if (gitRegex.match(args[0])) {
+			argumentsToPass[0] = 'git';
+			argumentsToPass.push(gitRegex.matched(1));
+			argumentsToPass.push(gitRegex.matched(2));
+		} else {
+			argumentsToPass.concat(args);
+		}
 
-    }
-    argumentsToPass.concat(argFlags);
-    Sys.command('haxelib', argumentsToPass);
-  }
-  @:command('update')
-  public function update(rest:Rest<String>) {
-		Sys.command('haxelib', ['update'].concat(cast rest).concat(getArgsForhaxe()));
-  }
-  @:command('remove', 'rm', 'uninstall')
-  public function remove(rest:Rest<String>) {
-		Sys.command('haxelib', ['remove'].concat(cast rest).concat(getArgsForhaxe()));
-  }
-  @:command('list', 'ls')
-  public function list(rest:Rest<String>) {
-		Sys.command('haxelib', ['list'].concat(cast rest).concat(getArgsForhaxe()));
-  }
+		}
+		argumentsToPass.concat(argFlags);
+		trace(argumentsToPass);
+		Sys.command('haxelib', argumentsToPass);
+	}
+	@:command('update')
+	public function update(rest:Rest<String>) {
+			Sys.command('haxelib', ['update'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+	@:command('remove', 'rm', 'uninstall')
+	public function remove(rest:Rest<String>) {
+			Sys.command('haxelib', ['remove'].concat(cast rest).concat(getArgsForhaxe()));
+	}
+	@:command('list', 'ls')
+	public function list(rest:Rest<String>) {
+			Sys.command('haxelib', ['list'].concat(cast rest).concat(getArgsForhaxe()));
+	}
 	@:command('set')
 	public function set(rest:Rest<String>) {
 		Sys.command('haxelib', ['set'].concat(cast rest).concat(getArgsForhaxe()));
@@ -208,7 +218,7 @@ class SilkCli {
 	@:command('version')
 	public function version(rest:Rest<String>) {
 		Sys.command('haxelib', ['version'].concat(cast rest).concat(getArgsForhaxe()));
-    Sys.println('Silk Version: 0.0.1');
+	Sys.println('Silk Version: 0.0.1');
 	}
 	@:command('submit')
 	public function submit(rest:Rest<String>) {
@@ -246,31 +256,122 @@ class SilkCli {
 	public function proxy(rest:Rest<String>) {
 		Sys.command('haxelib', ['proxy'].concat(cast rest).concat(getArgsForhaxe()));
 	}
-  function getArgsForhaxe():Array<String> {
-		var flagsAsArgs = [];
-		if (always) {
-			flagsAsArgs.push('--always');
+	@:command('why')
+	public function why(rest:Rest<String>) {
+		
+		// idk why i need to declare type here... isn't that what the cast is supposed to do? 
+		var goodArgs:Array<String> = cast (rest : Array<String>);
+		if (goodArgs[0] == null) 
+			throw 'Library must be specified';
+		if (goodArgs[1] == null)
+			throw 'Hxml file must be specified.';
+		var myHaxelib = File.getContent(goodArgs[1]);
+
+		var pathThing = scanForDep(myHaxelib, goodArgs[0]);
+		
+		
+		if (pathThing[0] == '~this') {
+			Sys.println('This is directly required by your project.');
+		} else if (pathThing.length == 0){
+			Sys.println("Couldn't find a reason (either silk couldn't find a reason or the project doesn't use "+ goodArgs[0] + " as a dependency.)");
+		} else {
+			var smellyName:String = 'Dependency structure:';
+			for (i in 0...pathThing.length) {
+				var thing = pathThing[i];
+				if (i != 0) {
+					smellyName += ',';
+				}
+				smellyName += ' ${thing}';
+			}
+			Sys.println(smellyName);
 		}
-		if (never) {
-			flagsAsArgs.push('--never');
+
+	}
+	function scanForDep(hxml:String, lib:String) {
+		var rep = hecks.getRepository();
+		for (l in hxml.split('\n')) {
+			var funnyRegex = ~/(?:-L|--library) ([A-Za-z0-9_\-.]+)(?::(git|(?:(?:[0-9]+)\.(?:[0-9]+)\.(?:[0-9]+)(?:-(?:[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?)))?/;
+			if (funnyRegex.match(l)) {
+				if (funnyRegex.matched(1) == lib) {
+					// wait a minute
+					return ['~this'];
+				}
+				var results = new List();
+				hecks.checkRec(rep, funnyRegex.matched(1), funnyRegex.matched(2), results, false);
+				var path = '';
+				if (!results.isEmpty())
+					path = results.first().dir;
+				if (path == '')
+					continue;
+				var haxelibData = Data.readData(File.getContent(path + '/haxelib.json'), CheckData);
+				var coolThingies = scanForDepFromLib(haxelibData, lib, [funnyRegex.matched(1)]);
+				// trace(coolThingies);
+				// note to self [] != [] because objects
+				if (coolThingies.length != 0)
+					return coolThingies;
+			}
 		}
-		if (flat) {
-			flagsAsArgs.push('--flat');
+		return [];
+	}
+	function scanForDepFromLib(libData:Infos, scanFor:String, path:Array<String>):Array<String> {
+		var rep = hecks.getRepository();
+		for (dep in libData.dependencies) {
+			var nuPath = path.copy();
+			nuPath.push(dep.name);
+			if (dep.name == scanFor) {
+				return nuPath;
+			}
+			var results = new List();
+			hecks.checkRec(rep, dep.name, null, results, false);
+			var pathe = '';
+			if (!results.isEmpty())
+				pathe = results.first().dir;
+			if (pathe == '')
+				continue;
+			var haxelibData = Data.readData(File.getContent(pathe + '/haxelib.json'), CheckData);
+			var scanResult = scanForDepFromLib(haxelibData, scanFor, nuPath);
+			if (scanResult != [])
+				return scanResult;
 		}
-		if (debug) {
-			flagsAsArgs.push('--debug');
-		}
-		if (quiet) {
-			flagsAsArgs.push('--quiet');
-		}
-		if (system) {
-			flagsAsArgs.push('--system');
-		}
-		if (skipdeps)
-			flagsAsArgs.push('--skip-dependencies');
-		if (global)
-			flagsAsArgs.push('--global');
-    return flagsAsArgs;
-  }
-  public function new() {}
+		return [];
+	}
+	function getArgsForhaxe():Array<String> {
+			var flagsAsArgs = [];
+			if (always) {
+				flagsAsArgs.push('--always');
+			}
+			if (never) {
+				flagsAsArgs.push('--never');
+			}
+			if (flat) {
+				flagsAsArgs.push('--flat');
+			}
+			if (debug) {
+				flagsAsArgs.push('--debug');
+			}
+			if (quiet) {
+				flagsAsArgs.push('--quiet');
+			}
+			if (system) {
+				flagsAsArgs.push('--system');
+			}
+			if (skipdeps)
+				flagsAsArgs.push('--skip-dependencies');
+			if (global)
+				flagsAsArgs.push('--global');
+		return flagsAsArgs;
+	}
+	public function new() {
+		hecks = new HaxelibMain();
+		hecks.settings = {
+			debug: debug,
+			quiet: quiet,
+			always: always,
+			never: never,
+			flat: flat,
+			global: global,
+			system: system,
+			skipDependencies: skipdeps,
+		};
+	}
 }
